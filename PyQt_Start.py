43160da18f807391bb86eafd7676ac5787208ca9
jeongji75 <land815@naver.com>
2024-09-13 17:40:52 +0900

    추가

---
+++ PyQt_Start.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)
+
+win = QWidget()
+win.show()
+
+app.exec_()
+
+import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)
+
+win = QWidget()
+win.show()
+
+app.exec_()
